58e10f255971273e9ee0c585ef79e029512e1685
mykola-bartkiv <bartkivmm93@gmail.com>
2021-02-07 14:54:29 +0200

    21. Creating Our First Model - closes #1

---
+++ models/Bootcamps.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+
+const BootcampShema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        maxlength: [50, 'Name can not be more than 50 characters'],
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a desription'],
+        unique: true,
+        maxlength: [500, 'Name can not be more than 500 characters'],
+    },
+    website: {
+        type: String,
+        match: [
+            /http?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS',
+        ],
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be more than 20 characters'],
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email',
+        ],
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address'],
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere',
+        },
+        formatAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
+    },
+    careers: {
+        type: [String],
+        required: true,
+        enum: [
+            'Web Developer',
+            'Mobile Developer',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other',
+        ],
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating must be at least 10'],
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg',
+    },
+    housing: {
+        type: Boolean,
+        default: false,
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false,
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false,
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampShema);
